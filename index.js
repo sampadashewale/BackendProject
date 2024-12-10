ece61ebcfb6192cdbeabf4fbab681673ac9d3075
sampada shewlae <sampadashewale1@gmail.com>
2024-12-10 15:58:21 +0530

    github data added

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 //const port = 2000
+const githubData = {
+    "message": "Not Found",
+    "documentation_url": "https://docs.github.com/rest",
+    "status": "404"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,6 +23,11 @@ app.get('/login', (req, res) => {
 app.get('/youtube', (req, res) => {
     res.send('<h1><center>youtube page Here</center></h1>')
 })
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
+})
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
